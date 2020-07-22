34e259b53c78f781f2433b36388b242c5a5de02d
Prakhar (wolfdale) <mywolfdale@gmail.com>
2020-07-22 21:45:21 +0530

    Kafka Producer Upgrade

--- producer/src/main/java/com/transaction/producer/KafkaConfigurations.java
+++ producer/src/main/java/com/transaction/producer/KafkaConfigurations.java
@@ -4,11 +4,9 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -22,13 +20,13 @@ public class KafkaConfigurations {
     @Value(value = "${kafka.bootstrap.server}")
     private String bootstrapAddress;
 
-    @Bean
-    public ProducerFactory<String, String> producerFactory() {
+    @Value(value = "${kafka.topic.name}")
+    public String topic;
 
+    private ProducerFactory<String, String> producerFactory() {
         log.info("Kafka broker address {} ", bootstrapAddress);
         HashMap<String, Object> configProperties = new HashMap<>();
-        configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                bootstrapAddress);
+        configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
@@ -40,4 +38,7 @@ public class KafkaConfigurations {
         return new KafkaTemplate<>(producerFactory(), true);
     }
 
+    public String getKafkaTopic() {
+        return this.topic;
+    }
 }
--- producer/src/main/java/com/transaction/producer/model/Transaction.java
+++ producer/src/main/java/com/transaction/producer/model/Transaction.java
@@ -1,35 +1,21 @@
 package com.transaction.producer.model;
 
 /**
- * POJO for Card Transaction.
+ * DTO representing Card Transaction.
  */
 public class Transaction {
     String uuid;
     String status;
+    String client;
 
-    public Transaction(String uuid, String status){
+    public Transaction(String uuid, String status, String client){
         this.uuid = uuid;
         this.status = status;
-    }
-
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
+        this.client = client;
     }
 
     @Override
     public String toString(){
-        return "[" + this.uuid + ", " + this.status + "]";
+        return "[" + this.uuid + ", " + this.status + ", " + this.client + "]";
     }
 }
